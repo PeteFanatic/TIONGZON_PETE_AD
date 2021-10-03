59ab429f90ac36ffebab3985fe1e5ba476bcdf89
Pete Tiongzon <pete.tiongzon@gmail.com>
2021-10-03 17:34:45 +0800

    API Refined

--- 3-nest/src/user/user.controller.ts
+++ 3-nest/src/user/user.controller.ts
@@ -10,45 +10,41 @@ export class UserController {
   }
 
   @Delete('/:id')
-  deleteUser(@Param("id") id:number) {
+  deleteUser(@Param("id") id:string) {
     return this.userService.deleteUser(id);
   }
 
   @Get('/:id')
-  getOne(@Param("id") id:number) {
-    return this.userService.getUser(id);
+  getOne(@Param("id") id:string) {
+    return this.userService.getOne(id);
   }
 
-  @Put('/replaceUser/:id')
-  replaceUser(@Param("id") id:number, @Body() body: any) {
-    return this.userService.replaceUser(id,body);
+
+  @Put('/:id')
+  replaceValuePut(@Param("id") id:string, @Body() body: any) {
+    return this.userService.replaceValuePut(id,body);
   }
+
 
   @Post('/register')
-  registerUser(@Body() body: any) {
-    return this.userService.registerUser(body);
+  register(@Body() body: any) {
+    return this.userService.register(body);
   }
 
-  @Patch("/replaceUser/:id")
-  register(@Body() body:any, @Param("id") id:number){
-    return this.userService.replaceUser(id,body);
+
+  @Patch("/:id")
+ replaceValuePatch(@Body() body:any, @Param("id") id:string){
+    return this.userService.replaceValuePatch(body,id);
   }
 
   @Post("/login")
-  loginUser(@Body("email") email:string , @Body("password") password:string){
-      return this.userService.loginUser(email,password);
+  login(@Body("email") email:string , @Body("password") password:string){
+      return this.userService.login(email,password);
   }
 
   @Get('/search/:id')
-  searchUser(@Param("id")id:number){
+  searchUser(@Param("id")id:string){
     return this.userService.searchUser(id);
   }
-
-  @Get('/logUser')
-  logUser() {
-    return this.userService.logAllUser();
-    // return;
-  }
-  //@Get('/user/:all')
   
 }
